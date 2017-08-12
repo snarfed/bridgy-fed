3928147414ffe6903af8d1d4cdad8c2f7fb1dbe6
Ryan Barrett <git@ryanb.org>
2017-08-12 16:51:48 -0700

    initial setup

---
+++ appengine_config.py
@@ -0,0 +1,20 @@
+"""Bridgy App Engine config.
+"""
+import os
+
+# Load packages from virtualenv
+# https://cloud.google.com/appengine/docs/python/tools/libraries27#vendoring
+from google.appengine.ext import vendor
+try:
+  vendor.add('local')
+except ValueError as e:
+  import logging
+  logging.warning("Couldn't set up App Engine vendor virtualenv! %s", e)
+
+from granary.appengine_config import *
+
+# Make requests and urllib3 play nice with App Engine.
+# https://github.com/snarfed/bridgy/issues/396
+# http://stackoverflow.com/questions/34574740
+from requests_toolbelt.adapters import appengine
+appengine.monkeypatch()
