27c2623700a94d1a73c2875ecd57abb6483bc38f
Ryan Barrett <git@ryanb.org>
2021-07-05 20:54:23 -0700

    flask: start porting render.py

--- render.py
+++ render.py
@@ -2,50 +2,46 @@
 """Renders mf2 proxy pages based on stored Responses."""
 import datetime
 
+from flask import Flask, request
 from granary import as2, atom, microformats2
+from oauth_dropins.webutil.appengine_config import ndb_client
 from oauth_dropins.webutil.handlers import cache_response
 from oauth_dropins.webutil import util
 from oauth_dropins.webutil.util import json_loads
-import webapp2
 
 import common
 from models import Response
 
 CACHE_TIME = datetime.timedelta(minutes=15)
 
+app = Flask(__name__)
 
-class RenderHandler(common.Handler):
-    """Fetches a stored Response and renders it as HTML."""
 
-    @cache_response(CACHE_TIME)
-    def get(self):
-        source = util.get_required_param(self, 'source')
-        target = util.get_required_param(self, 'target')
+@app.route('/render')
+# TODO
+# @cache_response(CACHE_TIME)
+def render():
+    source = request.args['source']
+    target = request.args['target']
 
-        id = '%s %s' % (source, target)
+    id = f'{source} {target}'
+    with ndb_client.context():
         resp = Response.get_by_id(id)
-        if not resp:
-            self.abort(404, 'No stored response for %s' % id)
+    if not resp:
+        return (f'No stored response for {id}', 404)
 
-        if resp.source_mf2:
-            as1 = microformats2.json_to_object(json_loads(resp.source_mf2))
-        elif resp.source_as2:
-            as1 = as2.to_as1(json_loads(resp.source_as2))
-        elif resp.source_atom:
-            as1 = atom.atom_to_activity(resp.source_atom)
-        else:
-            self.abort(404, 'Stored response for %s has no data' % id)
+    if resp.source_mf2:
+        as1 = microformats2.json_to_object(json_loads(resp.source_mf2))
+    elif resp.source_as2:
+        as1 = as2.to_as1(json_loads(resp.source_as2))
+    elif resp.source_atom:
+        as1 = atom.atom_to_activity(resp.source_atom)
+    else:
+        return (f'Stored response for {id} has no data', 404)
 
-        # add HTML meta redirect to source page. should trigger for end users in
-        # browsers but not for webmention receivers (hopefully).
-        html = microformats2.activities_to_html([as1])
-        utf8 = '<meta charset="utf-8">'
-        refresh = '<meta http-equiv="refresh" content="0;url=%s">' % source
-        html = html.replace(utf8, utf8 + '\n' + refresh)
-
-        self.response.write(html)
-
-
-ROUTES = [
-  ('/render', RenderHandler),
-]
+    # add HTML meta redirect to source page. should trigger for end users in
+    # browsers but not for webmention receivers (hopefully).
+    html = microformats2.activities_to_html([as1])
+    utf8 = '<meta charset="utf-8">'
+    refresh = f'<meta http-equiv="refresh" content="0;url={source}">'
+    return html.replace(utf8, utf8 + '\n' + refresh)
