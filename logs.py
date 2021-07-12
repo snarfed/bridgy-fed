73ec212c0bc66d0974ae68e06dd01984fe653e7e
Ryan Barrett <git@ryanb.org>
2021-07-12 13:59:27 -0700

    flask: temporarily remove log links from /responses
    ...until we port LogHandler to Flask.

--- logs.py
+++ logs.py
@@ -22,15 +22,10 @@ def responses():
     for r in responses:
         r.source_link = util.pretty_link(r.source())
         r.target_link = util.pretty_link(r.target())
-        r.log_url_path = '/log?' + urllib.parse.urlencode({
-          'key': r.key.id(),
-          'start_time': calendar.timegm(r.updated.timetuple()),
-        })
+        # TODO: uncomment once we've ported LogHandler to Flask
+        # r.log_url_path = '/log?' + urllib.parse.urlencode({
+        #   'key': r.key.id(),
+        #   'start_time': calendar.timegm(r.updated.timetuple()),
+        # })
 
-    print(f'@ {app.root_path} {app.template_folder} {app.jinja_loader.searchpath} {repr(app.jinja_loader)}')
     return render_template('responses.html', responses=responses)
-
-
-ROUTES = [
-    ('/log', LogHandler),
-]
