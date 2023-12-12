e8b79a0c0afa874101907c2747c2153bc2c16a80
Ryan Barrett <git@ryanb.org>
2023-12-11 16:17:05 -0800

    front page redesign: misc
    * follow instructions and code
    * vertical center buttons
    * more content

---
+++ static/index.js
@@ -0,0 +1,21 @@
+/**
+ * Code for the home page protocol picker.
+ */
+
+function update() {
+  // show matching instructions
+  // console.log($('.instruction'))
+  for (instr of $('.instruction')) {
+    let parts = instr.id.split('-')
+    let me = parts[1]
+    let them = parts[2]
+    // console.log(me, them, $(`#me-${me}`)[0].checked, $(`#them-${them}`)[0].checked)
+    instr.style.display =
+      ($(`#me-${me}`)[0].checked && $(`#them-${them}`)[0].checked)
+      ? 'block' : 'none'
+  }
+}
+
+addEventListener('DOMContentLoaded', () => {
+  $('input').on('change', update)
+})
