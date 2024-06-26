snarfed/bridgy-fed

commit a735396864029dc37d84a2639b30fec728ebd488

Protocol.translate_ids: add attachment.url
for https://github.com/snarfed/bridgy-fed/issues/461 , fixes AP => ATProto quote post of AP original post that was bridged

protocol.py
@@ -719,6 +719,7 @@ class Protocol:
                     translate(tag, 'url', translate_user_id)
             for att in as1.get_objects(o, 'attachments'):
                 translate(att, 'id', translate_object_id)
+                translate(att, 'url', translate_object_id)
 
         outer_obj = util.trim_nulls(outer_obj)
         if outer_obj.get('object', {}).keys() == {'id'}:

tests/test_protocol.py
@@ -536,7 +536,7 @@ class ProtocolTest(TestCase):
             'objectType': 'note',
             'attachments': [
                 {'id': 'other:o:fa:fake:123'},
-                {'url': 'fake:456'},
+                {'url': 'other:o:fa:fake:456'},
             ],
             'tags': [
                 {'objectType': 'mention', 'url': 'other:u:fake:alice'},
@@ -568,6 +568,10 @@ class ProtocolTest(TestCase):
                 'id': 'other:o:fa:fake:reply',
                 'inReplyTo': 'other:post',
             },
+            'attachments': [{
+                'objectType': 'note',
+                'url': 'other:post',
+            }],
         }, OtherFake.translate_ids({
             'objectType': 'activity',
             'verb': 'post',
@@ -576,6 +580,10 @@ class ProtocolTest(TestCase):
                 'id': 'fake:reply',
                 'inReplyTo': 'fake:post',
             },
+            'attachments': [{
+                'objectType': 'note',
+                'url': 'fake:post',
+            }],
         }))
 
     def test_convert_object_is_from_user_adds_source_links(self):

tests/test_atproto.py
@@ -698,6 +698,34 @@ class ATProtoTest(TestCase):
             }],
         })))
 
+    def test_convert_quote_post_translate_attachment_url_with_copy_id(self):
+        self.make_user_and_repo()
+
+        self.store_object(id='fake:orig-post', copies=[
+            Target(protocol='atproto', uri='at://did:plc:user/coll/tid'),
+        ])
+        self.repo.apply_writes([Write(action=Action.CREATE, collection='coll',
+                                      rkey='tid', record=NOTE_BSKY)])
+
+        self.assertEqual({
+            '$type': 'app.bsky.feed.post',
+            'text': '',
+            'createdAt': '2022-01-02T03:04:05.000Z',
+            'embed': {
+                '$type': 'app.bsky.embed.record',
+                'record': {
+                    'uri': 'at://did:plc:user/coll/tid',
+                    'cid': 'bafyreiccskuaccxa6zbaf7jeaiwyzg3pqtj3rg5qra653f5cmqilvgvejy',
+                },
+            },
+        }, ATProto.convert(Object(our_as1={
+            'objectType': 'note',
+            'attachments': [{
+                'objectType': 'note',
+                'url': 'fake:orig-post',
+            }],
+        })))
+
     def test_convert_actor_from_atproto_doesnt_add_self_label(self):
         self.assertEqual({
             '$type': 'app.bsky.actor.profile',

commit ac2df747a6c8a1d9883e09398f9434edda9f6d24

Protocol.translate_ids: fix user-visible quote post URL
broken by a735396, for #1154 and #461

protocol.py
@@ -723,7 +723,10 @@ class Protocol:
                     translate(tag, 'url', translate_user_id)
             for att in as1.get_objects(o, 'attachments'):
                 translate(att, 'id', translate_object_id)
-                translate(att, 'url', translate_object_id)
+                url = att.get('url')
+                if url and not att.get('id'):
+                    from_cls = Protocol.for_id(url)
+                    att['id'] = translate_object_id(from_=from_cls, to=to_cls, id=url)
 
         outer_obj = util.trim_nulls(outer_obj)
         if outer_obj.get('object', {}).keys() == {'id'}:

tests/test_activitypub.py
@@ -2557,6 +2557,35 @@ class ActivityPubUtilsTest(TestCase):
             'object': ACTOR,
         }, ActivityPub.convert(obj))
 
+    def test_convert_quote_post(self):
+        obj = Object(id='at://did:alice/app.bsky.feed.post/123', bsky={
+            '$type': 'app.bsky.feed.post',
+            'text': 'foo bar',
+            'embed': {
+                '$type': 'app.bsky.embed.record',
+                'record': {
+                    'cid': 'bafyreih...',
+                    'uri': 'at://did:bob/app.bsky.feed.post/456'
+                }
+            },
+        })
+
+        self.assert_equals({
+            'type': 'Note',
+            'id': 'https://bsky.brid.gy/convert/ap/at://did:alice/app.bsky.feed.post/123',
+            'url': 'http://localhost/r/https://bsky.app/profile/did:alice/post/123',
+            'content': '<p>foo bar<br><br>RE: <a href="https://bsky.app/profile/did:bob/post/456">https://bsky.app/profile/did:bob/post/456</a></p>',
+            'attributedTo': 'did:alice',
+            '_misskey_quote': 'https://bsky.brid.gy/convert/ap/at://did:bob/app.bsky.feed.post/456',
+            'quoteUrl': 'https://bsky.brid.gy/convert/ap/at://did:bob/app.bsky.feed.post/456',
+            'tag': [{
+                'type': 'Link',
+                'mediaType': as2.CONTENT_TYPE_LD_PROFILE,
+                'href': 'https://bsky.brid.gy/convert/ap/at://did:bob/app.bsky.feed.post/456',
+                'name': 'RE: https://bsky.app/profile/did:bob/post/456',
+            }],
+        }, ActivityPub.convert(obj), ignore=['contentMap', 'content_is_html', 'to'])
+
     def test_postprocess_as2_idempotent(self):
         for obj in (ACTOR, REPLY_OBJECT, REPLY_OBJECT_WRAPPED, REPLY,
                     NOTE_OBJECT, NOTE, MENTION_OBJECT, MENTION, LIKE,

tests/test_protocol.py
@@ -536,7 +536,8 @@ class ProtocolTest(TestCase):
             'objectType': 'note',
             'attachments': [
                 {'id': 'other:o:fa:fake:123'},
-                {'url': 'other:o:fa:fake:456'},
+                {'id': 'other:o:fa:fake:456',
+                 'url': 'fake:456'},
             ],
             'tags': [
                 {'objectType': 'mention', 'url': 'other:u:fake:alice'},
@@ -570,7 +571,8 @@ class ProtocolTest(TestCase):
             },
             'attachments': [{
                 'objectType': 'note',
-                'url': 'other:post',
+                'id': 'other:post',
+                'url': 'fake:post',
             }],
         }, OtherFake.translate_ids({
             'objectType': 'activity',