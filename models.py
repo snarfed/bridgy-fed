snarfed/bridgy-fed

commit c47f3640e6bcccff8efbea39291f97bed9d2879d

User.get_or_create: cut down extra puts

models.py
@@ -268,13 +268,13 @@ class User(StringIdModel, metaclass=ProtocolUserMeta):
                 if ((old_val is None and new_val is not None)
                         or (field == 'direct' and not old_val and new_val)):
                     setattr(user, field, new_val)
-                    user.put()
 
             if enabled_protocols := kwargs.get('enabled_protocols'):
                 user.enabled_protocols = (set(user.enabled_protocols)
                                           | set(enabled_protocols))
 
             if not propagate:
+                user.put()
                 return user
         else:
             if orig := get_original(id):

commit 0f14410cf2b81e4c9068c5ce0993d290886bf256

User.get_or_create: consolidate puts better
c47f364 caused lots of contention, this hopefully fixes that. https://console.cloud.google.com/errors/detail/CJm_4sDv9O-iKg;time=P1D;locations=global?project=bridgy-federated

models.py
@@ -262,20 +262,25 @@ class User(StringIdModel, metaclass=ProtocolUserMeta):
             user.existing = True
 
             # TODO: propagate more fields?
+            changed = False
             for field in ['direct', 'obj', 'obj_key']:
                 old_val = getattr(user, field, None)
                 new_val = kwargs.get(field)
                 if ((old_val is None and new_val is not None)
                         or (field == 'direct' and not old_val and new_val)):
                     setattr(user, field, new_val)
+                    changed = True
 
             if enabled_protocols := kwargs.get('enabled_protocols'):
                 user.enabled_protocols = (set(user.enabled_protocols)
                                           | set(enabled_protocols))
+                changed = True
 
             if not propagate:
-                user.put()
+                if changed:
+                    user.put()
                 return user
+
         else:
             if orig := get_original(id):
                 if orig.status and not allow_opt_out: