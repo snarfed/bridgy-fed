69a158aee5f7ae2f8a79ab092a594c14576e9fc2
Ryan Barrett <git@ryanb.org>
2024-08-12 13:51:18 -0700

    suppress bs4 DeprecationWarning, 'strip_cdata' option of HTMLParser
    local/lib/python3.11/site-packages/bs4/builder/_lxml.py:124: DeprecationWarning: The 'strip_cdata' option of HTMLParser() has never done anything and will eventually be removed.
    
    introduced in lxml 5.3.0, d31d21396c4fedfc1d036f51f18a6b8e7a5b1444

--- tests/testutil.py
+++ tests/testutil.py
@@ -310,6 +310,9 @@ class TestCase(unittest.TestCase, testutil.Asserts):
         # suppress a few warnings
         # local/lib/python3.9/site-packages/bs4/__init__.py:435: MarkupResemblesLocatorWarning: The input looks more like a filename than markup. You may want to open this file and pass the filehandle into Beautiful Soup.
         warnings.filterwarnings('ignore', category=MarkupResemblesLocatorWarning)
+        # local/lib/python3.11/site-packages/bs4/builder/_lxml.py:124: DeprecationWarning: The 'strip_cdata' option of HTMLParser() has never done anything and will eventually be removed.
+        warnings.filterwarnings('ignore', category=DeprecationWarning,
+                                message="The 'strip_cdata' option of HTMLParser")
 
         # arroba config
         os.environ.update({
