41662e0b2384b2573e5ca8f3b32dc181395f996d
Ryan Barrett <git@ryanb.org>
2024-10-02 12:18:32 -0700

    common.create_task: limit logged task body to 1000 chars

--- common.py
+++ common.py
@@ -363,7 +363,7 @@ def create_task(queue, delay=None, **params):
 
     parent = tasks_client.queue_path(appengine_info.APP_ID, TASKS_LOCATION, queue)
     task = tasks_client.create_task(parent=parent, task=task)
-    msg = f'Added {queue} task {task.name} delay {delay} {body}'
+    msg = f'Added {queue} task {task.name} delay {delay} {body[:1000]}'
     logger.info(msg)
     return msg, 202
 
