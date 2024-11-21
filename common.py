b77715fc3c8bc9f15ed5b4dbd2ccc675ea09d568
Ryan Barrett <git@ryanb.org>
2024-11-21 14:19:07 -0800

    create_task: stop logging JSON parameter values
    for #1149

--- common.py
+++ common.py
@@ -330,6 +330,7 @@ def create_task(queue, delay=None, **params):
     assert queue
     path = f'/queue/{queue}'
 
+    loggable = {k: '{...}' if isinstance(v, dict) else v for k, v in params.items()}
     params = {k: json_dumps(v, sort_keys=True) if isinstance(v, dict) else v
               for k, v in params.items()}
 
@@ -360,7 +361,6 @@ def create_task(queue, delay=None, **params):
 
     parent = tasks_client.queue_path(appengine_info.APP_ID, TASKS_LOCATION, queue)
     task = tasks_client.create_task(parent=parent, task=task)
-    loggable = {k: '{...}' if isinstance(v, dict) else v for k, v in params.items()}
     msg = f'Added {queue} task {task.name} delay {delay} {loggable}'
     logger.info(msg)
     return msg, 202
