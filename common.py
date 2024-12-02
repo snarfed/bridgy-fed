23e0323e60d0e4b80640469c35956138b069cbb1
Ryan Barrett <git@ryanb.org>
2024-12-02 11:54:16 -0800

    tweak common.DOMAIN_RE to avoid exponential backtracking
    hopefully fixes https://github.com/snarfed/bridgy-fed/security/code-scanning/53, https://github.com/snarfed/bridgy-fed/security/code-scanning/48

--- common.py
+++ common.py
@@ -35,7 +35,7 @@ logger = logging.getLogger(__name__)
 # https://stackoverflow.com/questions/10306690/what-is-a-regular-expression-which-will-match-a-valid-domain-name-without-a-subd
 #
 # TODO: preprocess with domain2idna, then narrow this to just [a-z0-9-]
-DOMAIN_RE = r'^([^/:;@?!\']+\.)+[^/:@_?!\']+$'
+DOMAIN_RE = r'^([^/:;@?!\'.]+\.)+[^/:@_?!\'.]+$'
 
 CONTENT_TYPE_HTML = 'text/html; charset=utf-8'
 
