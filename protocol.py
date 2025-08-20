snarfed/bridgy-fed

commit 734a8f519b9b23dc0b88d1b447a1b1595f20b28c

Protocol.receive: lower memcache lease expiration below receive queue's retry delay
for #1361

protocol.py
@@ -57,6 +57,8 @@ import notifications
 OBJECT_REFRESH_AGE = timedelta(days=30)
 DELETE_TASK_DELAY = timedelta(minutes=2)
 CREATE_MAX_AGE = timedelta(weeks=2)
+# WARNING: keep this below the receive queue's min_backoff_seconds in queue.yaml!
+MEMCACHE_LEASE_EXPIRATION = timedelta(seconds=25)
 
 # require a follow for users on these domains before we deliver anything from
 # them other than their profile
@@ -994,8 +996,10 @@ class Protocol:
 
         # lease this object, atomically
         memcache_key = activity_id_memcache_key(id)
-        leased = memcache.memcache.add(memcache_key, 'leased', noreply=False,
-                                       expire=5 * 60)  # 5 min
+        leased = memcache.memcache.add(
+            memcache_key, 'leased', noreply=False,
+            expire=MEMCACHE_LEASE_EXPIRATION.total_seconds())
+
         # short circuit if we've already seen this activity id.
         # (don't do this for bare objects since we need to check further down
         # whether they've been updated since we saw them last.)

queue.yaml
@@ -23,6 +23,7 @@ queue:
   max_concurrent_requests: 500
   retry_parameters:
     task_retry_limit: 4
+    # WARNING: keep this above protocol.MEMCACHE_LEASE_EXPIRATION!
     min_backoff_seconds: 30
     max_doublings: 4
 

tests/test_protocol.py
@@ -3570,11 +3570,13 @@ class ProtocolReceiveTest(TestCase):
         first = Thread(target=receive)
         second = Thread(target=receive)
 
+        memcache_key = protocol.activity_id_memcache_key('fake:post')
         with patch.object(OtherFake, 'send', side_effect=send):
             first.start()
             second.start()
             with at_send:
                 at_send.wait(10)  # timeout in seconds
+            self.assertEqual('leased', memcache.memcache.get(memcache_key))
             with continue_send:
                 continue_send.notify(1)
             first.join()
@@ -3582,6 +3584,9 @@ class ProtocolReceiveTest(TestCase):
 
         # only one receive call should try to send
         self.assertEqual([('other:alice:target', post_as1)], OtherFake.sent)
+
+        lease_key = protocol.activity_id_memcache_key('fake:post')
+        self.assertEqual('done', memcache.memcache.get(memcache_key))
 
     @patch('protocol.LIMITED_DOMAINS', ['lim.it'])
     @patch('requests.get')

commit ee9f114228be46ed855363c959f53d1d0c0e4d3e

Summary: Protocol.receive: memcache bug fix, expire needs to be an integer
bug was introduced in 734a8f5. tests didn't catch it because MockMemcacheClient didn't check for that. fixed that in https://github.com/pinterest/pymemcache/pull/647.

protocol.py
@@ -998,7 +998,7 @@ class Protocol:
         memcache_key = activity_id_memcache_key(id)
         leased = memcache.memcache.add(
             memcache_key, 'leased', noreply=False,
-            expire=MEMCACHE_LEASE_EXPIRATION.total_seconds())
+            expire=int(MEMCACHE_LEASE_EXPIRATION.total_seconds()))
 
         # short circuit if we've already seen this activity id.
         # (don't do this for bare objects since we need to check further down