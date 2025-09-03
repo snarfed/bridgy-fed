snarfed/bridgy-fed

commit c862307cf4d8402ca52da390121e7027ab7cf091

ActivityPub.inbox: don't delete Undo of Follow
for #2048, #1631, #1361

activitypub.py
@@ -1257,8 +1257,8 @@ def inbox(protocol=None, id=None):
     # do we support this object type?
     # (this logic is duplicated in Protocol.check_supported)
     obj = as1.get_object(activity)
+    inner_type = obj.get('type')
     if type := activity.get('type'):
-        inner_type = as1.object_type(obj) or ''
         if (type not in ActivityPub.SUPPORTED_AS2_TYPES or
             (type in as2.CRUD_VERBS
              and inner_type
@@ -1337,7 +1337,10 @@ def inbox(protocol=None, id=None):
         if user and not user.existing:
             logger.info(f'Automatically enabled AP server actor {actor_id}')
 
-    delay = DELETE_TASK_DELAY if type in ('Delete', 'Undo') else None
+    delay = None
+    if type == 'Delete' or (type == 'Undo' and inner_type != 'Follow'):
+        delay = DELETE_TASK_DELAY
+
     return create_task(queue='receive', id=id, as2=activity,
                        source_protocol=ActivityPub.LABEL, authed_as=authed_as,
                        received_at=util.now().isoformat(), delay=delay)

protocol.py
@@ -55,7 +55,7 @@ from models import (
 import notifications
 
 OBJECT_REFRESH_AGE = timedelta(days=30)
-DELETE_TASK_DELAY = timedelta(minutes=2)
+DELETE_TASK_DELAY = timedelta(minutes=1)
 CREATE_MAX_AGE = timedelta(weeks=2)
 # WARNING: keep this below the receive queue's min_backoff_seconds in queue.yaml!
 MEMCACHE_LEASE_EXPIRATION = timedelta(seconds=25)

tests/test_activitypub.py
@@ -676,6 +676,17 @@ class ActivityPubTest(TestCase):
                          received_at='2022-01-02T03:04:05+00:00',
                          eta_seconds=delayed_eta)
 
+    @patch('oauth_dropins.webutil.appengine_config.tasks_client.create_task')
+    def test_inbox_undo_follow_receive_task_no_delay(self, mock_create_task, *mocks):
+        common.RUN_TASKS_INLINE = False
+
+        self.make_user(ACTOR['id'], cls=ActivityPub, obj_as2=ACTOR)
+        resp = self.post('/ap/sharedInbox', json=UNDO_FOLLOW_WRAPPED)
+        self.assert_task(mock_create_task, 'receive', id=UNDO_FOLLOW_WRAPPED['id'],
+                         source_protocol='activitypub', as2=UNDO_FOLLOW_WRAPPED,
+                         authed_as=ACTOR['id'],
+                         received_at='2022-01-02T03:04:05+00:00')
+
     def test_inbox_reply_object(self, mock_head, mock_get, mock_post):
         self._test_inbox_reply(REPLY_OBJECT, mock_head, mock_get, mock_post)
 

tests/test_protocol.py
@@ -1256,7 +1256,6 @@ class ProtocolReceiveTest(TestCase):
     def test_reply_to_non_bridged_post_with_mention_fails_for_retry(self, mock_send):
         self.user.enabled_protocols = ['atproto']
         self.user.put()
-
         self.alice.enabled_protocols = ['atproto']
         self.alice.put()
 
@@ -1284,8 +1283,6 @@ class ProtocolReceiveTest(TestCase):
         self.assertEqual([], OtherFake.sent)
 
     def test_reply_to_non_bridged_post_with_enabled_protocol_fails_for_retry(self):
-        self.make_user(id='fa.brid.gy', cls=Web)
-
         self.alice.enabled_protocols = ['efake']
         self.alice.put()
         self.bob.enabled_protocols = ['efake']

commit e733035bdb7091f7acf17ff677d56ef06cf1ff81

ActivityPub.inbox: handle Deletes of Tombstone objects
originally broken by c862307cf4d8402ca52da390121e7027ab7cf091, caught by @winterlapis and @pado3 in #2065. thank you both!

activitypub.py
@@ -1289,7 +1289,7 @@ def inbox(protocol=None, id=None):
     inner_type = obj.get('type')
     if type := activity.get('type'):
         if (type not in ActivityPub.SUPPORTED_AS2_TYPES or
-            (type in as2.CRUD_VERBS
+            (type in ('Create', 'Update')
              and inner_type
              and inner_type not in ActivityPub.SUPPORTED_AS2_TYPES)):
             error(f"Bridgy Fed for ActivityPub doesn't support {type} {inner_type} yet: {json_dumps(activity, indent=2)}", status=204)

tests/test_integrations.py
@@ -1038,7 +1038,13 @@ To disable these messages, reply with the text 'mute'.""",
             'type': 'Delete',
             'id': 'https://inst/delete',
             'actor': 'https://inst/alice',
-            'object': 'https://inst/post',
+            # some fediverse servers use Tombstones as their Delete objects,
+            # eg Threads and some Mastodon versions
+            # https://github.com/snarfed/bridgy-fed/issues/2065
+            'object': {
+                'type': 'Tombstone',
+                'id': 'https://inst/post',
+            },
         }
         body = json_dumps(delete)
         headers = sign('/ap/sharedInbox', body, key_id='https://inst/alice')
@@ -1046,6 +1052,7 @@ To disable these messages, reply with the text 'mute'.""",
         self.assertEqual(202, resp.status_code)
 
         # check that the record was deleted from the repo
+        repo = self.storage.load_repo('did:plc:alice')
         self.assertIsNone(repo.get_record('app.bsky.feed.post', tid))
 
     @patch('requests.post', return_value=requests_response(''))