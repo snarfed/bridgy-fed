f3cdac967b2d0c91c00032dd1c1cec14024fd362
Ryan Barrett <git@ryanb.org>
2025-09-06 21:18:15 -0700

    UIProtocol: stub out methods that Protocol raises NotImplementedError for
    this thing is such an awkward wart. I should just get rid of it instead.

--- ui.py
+++ ui.py
@@ -8,3 +8,24 @@ from protocol import Protocol
 
 class UIProtocol(User, Protocol):
     LABEL = 'ui'
+
+    @classmethod
+    def handle_to_id(cls, handle):
+        return None
+
+    @classmethod
+    def create_for(cls, user):
+        raise ValueError()
+
+    @classmethod
+    def send(to_cls, *args, **kwargs):
+        return False
+
+    @classmethod
+    def fetch(cls, obj, **kwargs):
+        return False
+
+    @classmethod
+    def target_for(cls, obj, **kwargs):
+        return None
+
