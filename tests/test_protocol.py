snarfed/bridgy-fed

commit 4d1ad68d49e3148fd922d379e778c789f475ff6e

Protocol.receive: handle updates even when memcache lease is 'done'
for #2507

[deploy]

protocol.py
@@ -1206,10 +1206,23 @@ class Protocol:
             expire=int(MEMCACHE_LEASE_EXPIRATION.total_seconds()))
 
         # short circuit if we've already seen this activity id
-        if ('force' not in request.values
-            and (not leased
-                 or (obj.new is False and obj.changed is False))):
-            error(f'Already seen', status=204)
+        #
+        # * 'leased' (25s): if add failed and the value is 'leased', another task is
+        #   processing this same id right now, so skip.
+        #
+        # * 'done' (1w): set at the end of receive below. if it's 'done' here, or
+        #   we just have the obj in the datastore, and content hasn't changed, skip.
+        #   if content has changed, fall through to process the update.
+        if 'force' not in request.values:
+            prior = None
+            if not leased and (prior := memcache.memcache.get(memcache_key)):
+                if isinstance(prior, bytes):
+                    prior = prior.decode()
+                if prior == 'leased':
+                    error('Already in progress', status=204)
+
+            if (prior == 'done' or obj.new is False) and obj.changed is False:
+                error('Already seen', status=204)
 
         pruned = {k: v for k, v in obj.as1.items()
                   if k not in ('contentMap', 'replies', 'signature')}

tests/test_protocol.py
@@ -2704,6 +2704,42 @@ class ProtocolReceiveTest(TestCase):
             'fake:post#bridgy-fed-update-2022-01-02T03:04:05+00:00'))
         self.assertEqual([], OtherFake.sent)
 
+    def test_update_post_bare_object_memcache_lease_done(self):
+        """Previous receive left 'done' in memcache; update should still process."""
+        self.make_followers()
+
+        post_as1 = {
+            'id': 'fake:post',
+            'objectType': 'note',
+            'author': 'fake:user',
+            'content': 'first',
+        }
+        self.store_object(id='fake:post', our_as1=post_as1, source_protocol='fake',
+                          copies=[Target(uri='other:post', protocol='other')])
+        memcache.memcache.set(protocol.activity_id_memcache_key('fake:post'), b'done')
+
+        post_as1['content'] = 'second'
+        _, code = Fake.receive_as1(post_as1, new=False, changed=True)
+        self.assertEqual(202, code)
+
+        update = {
+            'objectType': 'activity',
+            'verb': 'update',
+            'id': 'fake:post#bridgy-fed-update-2022-01-02T03:04:05+00:00',
+            'actor': 'fake:user',
+            'object': {
+                'objectType': 'note',
+                'id': 'fake:post',
+                'author': 'fake:user',
+                'content': 'second',
+                'updated': '2022-01-02T03:04:05+00:00',
+            },
+        }
+        self.assert_equals([
+            ('other:alice:target', update),
+            ('other:bob:target', update),
+        ], OtherFake.sent)
+
     def test_update_post_fetch_object(self):
         post = {
             'id': 'fake:post',

commit 3cef36442d9589b1319c20159b13a2eec9172c8a

Protocol.receive: fix regression that broke our activity de-duping
breakage came from 4d1ad68d49e3148fd922d379e778c789f475ff6e

for #2523 [deploy]

protocol.py
@@ -1211,8 +1211,9 @@ class Protocol:
         #   processing this same id right now, so skip.
         #
         # * 'done' (1w): set at the end of receive below. if it's 'done' here, or
-        #   we just have the obj in the datastore, and content hasn't changed, skip.
-        #   if content has changed, fall through to process the update.
+        #   we just have the obj in the datastore, skip unless content has changed
+        #   (changed is True). changed is often None here, eg a duplicate inbox
+        #   delivery, so we deliberately check `is not True`, not `is False`.
         if 'force' not in request.values:
             prior = None
             if not leased and (prior := memcache.memcache.get(memcache_key)):
@@ -1220,7 +1221,7 @@ class Protocol:
                 if prior == 'leased':
                     error('Already in progress', status=204)
 
-            if (prior == 'done' or obj.new is False) and obj.changed is False:
+            if (prior == 'done' or obj.new is False) and obj.changed is not True:
                 error('Already seen', status=204)
 
         pruned = {k: v for k, v in obj.as1.items()

tests/test_protocol.py
@@ -2740,6 +2740,57 @@ class ProtocolReceiveTest(TestCase):
             ('other:bob:target', update),
         ], OtherFake.sent)
 
+    def test_post_memcache_lease_done_not_changed(self):
+        """Duplicate delivery after a prior receive completed; should skip.
+
+        Reproduces duplicate posts: the prior receive left 'done' in memcache,
+        and this delivery has changed unset (None), not False.
+        https://github.com/snarfed/bridgy-fed/issues/2507
+        """
+        self.make_followers()
+
+        post = {
+            'objectType': 'activity',
+            'verb': 'post',
+            'id': 'fake:post:activity',
+            'actor': 'fake:user',
+            'object': {
+                'objectType': 'note',
+                'id': 'fake:post',
+                'author': 'fake:user',
+                'content': 'first',
+            },
+        }
+        memcache.memcache.set(
+            protocol.activity_id_memcache_key('fake:post:activity'), 'done')
+
+        _, code = Fake.receive_as1(post)
+        self.assertEqual(204, code)
+        self.assertEqual([], OtherFake.sent)
+
+    def test_post_memcache_lease_in_progress(self):
+        """Another task is processing this same id right now; should skip."""
+        self.make_followers()
+
+        post = {
+            'objectType': 'activity',
+            'verb': 'post',
+            'id': 'fake:post:activity',
+            'actor': 'fake:user',
+            'object': {
+                'objectType': 'note',
+                'id': 'fake:post',
+                'author': 'fake:user',
+                'content': 'first',
+            },
+        }
+        memcache.memcache.set(
+            protocol.activity_id_memcache_key('fake:post:activity'), 'leased')
+
+        _, code = Fake.receive_as1(post)
+        self.assertEqual(204, code)
+        self.assertEqual([], OtherFake.sent)
+
     def test_update_post_fetch_object(self):
         post = {
             'id': 'fake:post',