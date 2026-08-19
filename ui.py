ff7f08bbefc2cbe9ec09f880c0a4e22759517d15
Ryan Barrett <git@ryanb.org>
2026-08-19 13:36:38 -0700

    noop: update UIProtocol docstring, describe plan to tighten it up
    in preparation for the Mastodon API (#2493) storing interactions with unbridged users as Objects directly in the datastore

--- ui.py
+++ ui.py
@@ -1,6 +1,24 @@
-"""Stub UI Protocol class, for objects created in the UI.
+"""Stub protocol class for internal objects, ie objects that we create ourselves.
 
-Needed for serving /convert/ui/web/... requests.
+"UI" isn 't an ideal name. It now includes internal objects broadly, eg responses
+that users create via the Mastodon API, not just objects created via our own UI(s).
+
+Examples of where we currently create these:
+
+* Follows generated in follow.py (except their ids are often URLs instead)
+* Responses generated in pages.respond_*
+* Interactions with unbridged users via our Mastodon and PDS APIs.
+
+(DMs we send in dms.py use web URLs instead.)
+
+* Ids are ``ui:...``, opaque and protocol independent. Nothing should parse them.
+* ``source_protocol`` is ``ui``.
+* ``users` should be populated with the *author's* key.
+* Get the author's protocol via :meth:`models.Object.owner_protocol`, which uses
+  ``users` if available, then falls back to ``author``/``actor`` ids.
+* :meth:`protocol.Protocol.translate_ids` etc generates ids on the author's protocol
+  subdomain, eg ``https://bsky.brid.gy/convert/ap/ui:...``, which we serve from
+  :func:`convert.convert`.
 """
 from webutil import util
 
